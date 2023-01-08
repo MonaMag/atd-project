2ec78717df48f85c1e3415c1718d33a8e02aaa7e
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2023-01-08 03:15:42 +0300

    Add Button ui kit component.

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,11 +1,76 @@
-import React from "react";
+import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
 import cls from './Button.module.css';
+import { classNames, Mods } from '../../classNames/classNames';
 
-
-export const Button = () => {
-    return (
-        <div className={cls.btn}>
-            <button>Кнопка</button>
-        </div>
-    )
+export enum ButtonTheme {
+    CLEAR = 'clear',
+    CLEAR_INVERTED = 'clearInverted',
+    CLEAR_RED = 'clearRed',
+    OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_HOVERED = 'backgroundHovered',
+    BACKGROUND_RED = 'backgroundRed',
+    BACKGROUND_BLUE = 'backgroundBlue',
+    BACKGROUND_GREEN = 'backgroundGreen',
 }
+
+export enum ButtonSize {
+    XS = 'size_xs',
+    S = 'size_s',
+    M = 'size_m',
+    L = 'size_l',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    /**
+     * Тема кнопки. Отвечает за визуал (в рамке, без стилей, противоположный теме приложения цвет и тд)
+     */
+    theme?: ButtonTheme;
+    /**
+     * Размер кнопки в соответствии с дизайн системой
+     */
+    size?: ButtonSize;
+    /**
+     * Флаг, отвечающий за работу кнопки
+     */
+    disabled?: boolean;
+    /**
+     * Содержимое кнопки
+     */
+    children?: ReactNode;
+    /**
+     * Увеличивает кнопку на всю свободную ширину
+     */
+    fullWidth?: boolean;
+}
+
+export const Button = memo((props: ButtonProps) => {
+    const {
+        className,
+        children,
+        theme = ButtonTheme.OUTLINE,
+        disabled,
+        fullWidth,
+        size = ButtonSize.L,
+        ...otherProps
+    } = props;
+
+    const mods: Mods = {
+        [cls[theme]]: true,
+        [cls[size]]: true,
+        [cls.disabled]: disabled,
+        [cls.fullWidth]: fullWidth,
+    };
+
+    return (
+        <button
+            type="button"
+            className={classNames(cls.Button, mods, [className])}
+            disabled={disabled}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+});
