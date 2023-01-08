1a9c0de93d21b1987f2db135e5ac18ec68c89015
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2023-01-08 15:34:46 +0300

    Add Icon ui kit component.

---
+++ src/entities/User/index.ts
@@ -0,0 +1,5 @@
+export {userActions, userReducer} from './model/slice/userSlice';
+export {getUserAuthData} from './model/selectors/getUserAuthData';
+export {getUserInited} from './model/selectors/getUserInited';
+export {UserRole} from './model/consts/userConsts';
+export type {UserSchema} from './model/types/user';
